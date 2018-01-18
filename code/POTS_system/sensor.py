25ee3cc2176bb1c02093971d7c77c39029a24af4
agoodstudent <31169688+agoodstudent@users.noreply.github.com>
2018-01-18 17:54:59 -0500

    sensor

---
+++ code/POTS_system/sensor.py
@@ -0,0 +1,17 @@
+import math
+from vegtable import vegtable
+
+class sensor:
+    '''This is part of a smart street light'''
+
+    def __init__(self):
+        '''Intializes the light'''
+        
+
+    def Detect_Movement(self):
+        '''The range of movement the sensor can detect'''
+        return none
+
+    def Send_Signal(self):
+        '''This is how a sensor relays information'''
+        return none
