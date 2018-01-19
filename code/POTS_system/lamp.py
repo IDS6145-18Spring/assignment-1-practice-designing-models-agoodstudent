5f3861a16332fb8afcdc955e8f2ff5bb98e25817
agoodstudent <31169688+agoodstudent@users.noreply.github.com>
2018-01-18 19:54:53 -0500

    added lamp code

---
+++ code/POTS_system/lamp.py
@@ -0,0 +1,15 @@
+
+
+class Lamp
+
+    def__init_(self)
+    '''hardware containing bulb and cover'''
+
+    def bulb(self):
+        '''device that lights the streets'''
+        return none
+
+    def cover(self):
+        '''protects bulb from damage'''
+        return none
+
