3cfe18a7bf3e82f39cc121eef18f083b47ce9bf0
agoodstudent <31169688+agoodstudent@users.noreply.github.com>
2018-01-18 19:40:57 -0500

    network added

---
+++ code/POTS_system/network.py
@@ -0,0 +1,20 @@
+
+
+class Network:
+
+
+    def __init__(self):
+        '''Distributes data across broadband'''
+        
+
+    def Receive Signal(self):
+        '''Ability to receive messages from sensors and other inputs'''
+        return none
+
+    def Send_Signal(self):
+        '''This is how the network relays information'''
+        return none
+
+    def Process_Data (self):
+        '''Internal CPU and the cloud work to build better network'''
+        return none
